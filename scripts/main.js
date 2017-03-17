1928d718cff88763b17cd0cfa2d7b23901bcb53a
tariqksoliman <tariqksoliman@gmail.com>
2017-03-17 16:16:21 -0700

    Detect WebGL

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,16 @@
-var audioAnalyser = new AudioAnalyser();
-audioAnalyser.init();
+if ( ! Detector.webgl ) {
+    Detector.addGetWebGLMessage();
+}
+else {
+    var audioAnalyser = new AudioAnalyser();
+    audioAnalyser.init();
 
-var dragDropUpload = new DragDropUpload();
-dragDropUpload.init( audioAnalyser );
+    var dragDropUpload = new DragDropUpload();
+    dragDropUpload.init( audioAnalyser );
 
-var view = new View();
-view.init( audioAnalyser );
+    var view = new View();
+    view.init( audioAnalyser );
 
-var controller = new Controller();
-controller.init( audioAnalyser, view );
+    var controller = new Controller();
+    controller.init( audioAnalyser, view );
+}
